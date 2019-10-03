253183a9bc01b26367c663b8b2972111af193573
Shivam Kumar <shivamkumar9530@gmail.com>
2019-10-02 23:23:22 -0400

    firstcommit

--- lab3/app.js
+++ lab3/app.js
@@ -10,6 +10,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
 
 // view engine setup
--- lab3/routes/index.js
+++ lab3/routes/index.js
@@ -4,7 +4,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
+
     res.render('index', { title: 'Express' });
+
 });
 
+router.get('/shivam', function (req, res) {
+
+    res.render('Shivam', { title: 'Shivam' });
+
+});
+
+router.get('/pankaj', function (req, res) {
+
+    res.render('Pankaj', { title: 'Pankaj' });
+
+});
+
+router.get('/agyarani', function (req, res) {
+
+    res.render('AgyaRani', { title: 'AgyaRani' });
+
+});
+
+router.get('/sukhdevsingh', function (req, res) {
+
+    res.render('SukhdevSingh', { title: 'SukhdevSingh' });
+
+});
 module.exports = router;
